0079b0837f4f3736214cfb5c12eb1b2e14a54c8a
Dawid <davidowsky9519@gmail.com>
2023-01-07 17:32:57 +0100

    1of3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const pageBody = document.querySelector('body');
+let mainInterval;
 
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const changecolor = () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  mainInterval = setInterval(() => {
+    pageBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+startBtn.addEventListener('click', changecolor);
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(mainInterval);
+});
