199d7e9c90b4e44c380fd844cfba8c39fc49edda
Dawid <davidowsky9519@gmail.com>
2023-01-12 20:31:12 +0100

    3/3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+
+const formDelay = document.querySelector('[name="delay"]');
+const formStep = document.querySelector('[name="step"]');
+const formAmount = document.querySelector('[name="amount"]');
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const getPromise = e => {
+  e.preventDefault();
+  let newDelay = Number.parseInt(formDelay.value);
+  let newStep = Number.parseInt(formStep.value);
+
+  for (let i = 0; i < formAmount.value; i++) {
+    createPromise(i + 1, newDelay + newStep * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+};
+
+form.addEventListener('submit', getPromise);
